cd31e9edc57edb4ab4001c4fae0de30c656c359e
wewe1 <wewe1@DESKTOP-5S1ABLU>
2019-10-07 22:58:04 +0900

    생성자, 필드, 메소드 생성

---
+++ sakila/src/sakila/address/model/Country.java
@@ -0,0 +1,14 @@
+package sakila.address.model;
+
+public class Country {
+	// 생성자 생성
+	public Country() {
+		
+	}
+	// 필드 생성
+	private int countryId;
+	private String country;
+	private String lastUpdate;
+	
+	//set, get 메소드 생성
+}
